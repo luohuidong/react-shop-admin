7aaae6bfe5f7527cdf0767c22be1f0917cbfed2a
luohuidong <luohuidong01@126.com>
2019-01-09 23:08:42 +0800

    supported async/await

--- webpack/loader/babel.js
+++ webpack/loader/babel.js
@@ -7,7 +7,8 @@ module.exports = {
       presets: ['@babel/preset-env', '@babel/preset-react'],
       plugins: [
         '@babel/plugin-proposal-class-properties',
-        '@babel/plugin-transform-arrow-functions'
+        '@babel/plugin-transform-arrow-functions',
+        '@babel/plugin-transform-async-to-generator'
       ]
     }
   }
