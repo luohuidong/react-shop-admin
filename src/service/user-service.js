12b70d70fb5123f19ef50138e408b59bf0790efb
luohuidong <luohuidong01@126.com>
2019-01-10 20:23:58 +0800

    完善用户登录登出请求函数

--- src/service/user-service.js
+++ src/service/user-service.js
@@ -1,27 +1,43 @@
-import MUtil from 'util';
+import { post } from 'util/request';
 
-const { request } = new MUtil();
+/**
+ * 用户登录
+ * @param {string} username 账号
+ * @param {string} password 密码
+ */
+function requestUserLogin(username, password) {
+  const url = '/manage/user/login.do';
+  const data = {
+    username,
+    password,
+  };
+  return post(url, data).then(response => {
+    const { status, data, msg } = response.data;
 
-class User {
-  // 用户登录
-  login(loginInfo) {
-    const param = {
-      type: 'post',
-      url: '/user/login.do',
-      data: {
-        username: loginInfo.userName,
-        password: loginInfo.password
-      }
-    };
-    return request(param);
-  }
-
-  logout() {
-    return request({
-      type: 'post',
-      url: '/user/logout.do'
-    });
-  }
+    if (status === 0) {
+      return data;
+    } else {
+      return Promise.reject(msg);
+    }
+  });
 }
 
-export default User;
+/**
+ * 用户登出
+ */
+function userLogout() {
+  const url = '/user/logout.do';
+  return post(url).then(response => {
+    const { status } = response.data;
+    if (status === 0) {
+      return '退出登录成功';
+    } else {
+      return Promise.reject('推出登录失败');
+    }
+  });
+}
+
+export {
+  requestUserLogin,
+  userLogout
+};
