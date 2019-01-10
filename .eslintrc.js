539a294ddb75601cb5aa0054495bb6b301a88551
luohuidong <luohuidong01@126.com>
2019-01-10 20:18:57 +0800

    添加 switch 校验规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,8 +30,10 @@ module.exports = {
     "strict": 0,
     "indent": [
       "error",
-      2
+      2,
+      {"SwitchCase": 1}
     ],
+    
     "linebreak-style": [
       "error",
       "unix"
