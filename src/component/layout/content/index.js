dc1d8b8999dbfdca1c7a12ef71ef11a76bc034e7
luohuidong <luohuidong01@126.com>
2019-01-12 01:24:24 +0800

    将面包屑去掉

--- src/component/layout/content/index.js
+++ src/component/layout/content/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Layout, Breadcrumb } from 'antd';
+import { Layout } from 'antd';
 
 class Content extends React.PureComponent {
   render () {
     return (
-      <Layout.Content style={{ margin: '0 16px' }}>
-        <Breadcrumb style={{ margin: '16px 0' }}>
-          <Breadcrumb.Item>User</Breadcrumb.Item>
-          <Breadcrumb.Item>Bill</Breadcrumb.Item>
-        </Breadcrumb>
-        <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
+      <Layout.Content style={{ margin: '16px' }}>
+        <div style={{ padding: 24, minHeight: 360 }}>
           {this.props.children}
         </div>
       </Layout.Content>
