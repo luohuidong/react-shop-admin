54d298cf929e0eff7ef758fb1ec1b084af272af7
luohuidong <luohuidong01@126.com>
2019-01-17 19:19:54 +0800

    配置 Redux DevTools

--- src/store.js
+++ src/store.js
@@ -1,11 +1,17 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import { reducer as pageReducer } from './page/index';
 
 const reducer = combineReducers({ ...pageReducer });
 
-const store = createStore(
-  reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const middleware = [
+  thunk,
+];
+
+const store = createStore(reducer, composeEnhancers(
+  applyMiddleware(...middleware)
+));
 
 export default store;
