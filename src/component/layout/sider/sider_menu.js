347ed13196420a395855948fa4aa90b211f87bc9
luohuidong <luohuidong01@126.com>
2019-01-18 23:41:11 +0800

    细微改动

--- src/component/layout/sider/sider_menu.js
+++ src/component/layout/sider/sider_menu.js
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom';
 
 const SubMenu = Menu.SubMenu;
 
-class SiderMenu extends React.Component {
+class SiderMenu extends React.PureComponent {
   render() {
     return (
       <Menu theme='dark' defaultSelectedKeys={['1']} mode='vertical'>
         <Menu.Item key="1">
-          <Link key='a' to="/"><Icon type="pie-chart" />首页</Link>
+          <Link to="/"><Icon type="pie-chart" />首页</Link>
         </Menu.Item>
 
         <SubMenu
@@ -17,10 +17,10 @@ class SiderMenu extends React.Component {
           title={<span><Icon type="user" /><span>商品</span></span>}
         >
           <Menu.Item key="3">
-            <Link key='a' to="/product"><Icon type="pie-chart" />商品管理</Link>
+            <Link to="/product"><Icon type="pie-chart" />商品管理</Link>
           </Menu.Item>
           <Menu.Item key="4">
-            <Link key='a' to="/product-category"><Icon type="pie-chart" />品类管理</Link>
+            <Link to="/product-category"><Icon type="pie-chart" />品类管理</Link>
           </Menu.Item>
         </SubMenu>
 
@@ -29,7 +29,7 @@ class SiderMenu extends React.Component {
           title={<span><Icon type="pie-chart" /><span>订单</span></span>}
         >
           <Menu.Item key="6">
-            <Link key='a' to="/order"><Icon type="pie-chart" />订单管理</Link>
+            <Link to="/order"><Icon type="pie-chart" />订单管理</Link>
           </Menu.Item>
         </SubMenu>
 
@@ -38,7 +38,7 @@ class SiderMenu extends React.Component {
           title={<span><Icon type="team" /><span>用户</span></span>}
         >
           <Menu.Item key="7">
-            <Link key='a' to="/order"><Icon type="pie-chart" />用户管理</Link>
+            <Link to="/user"><Icon type="pie-chart" />用户管理</Link>
           </Menu.Item>
         </SubMenu>
 
