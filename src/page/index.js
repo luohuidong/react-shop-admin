2510943bcc7d50af0656c355afd9cc7213814fe7
luohuidong <luohuidong01@126.com>
2019-01-18 23:40:09 +0800

    在全局 reducer 中注册首页 reducer

--- src/page/index.js
+++ src/page/index.js
@@ -1,7 +1,9 @@
 import { reducer as LoginReducer } from './login/index';
+import { reducer as HomeReducer } from './home/index';
 
 const reducer = {
   login: LoginReducer,
+  home: HomeReducer
 };
 
 export {
