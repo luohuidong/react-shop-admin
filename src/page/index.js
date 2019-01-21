a6b31e2998b1ba52d29f0e87a5d1159be5ffaef5
luohuidong <luohuidong01@126.com>
2019-01-22 00:06:34 +0800

    新增商品相关 reducer

--- src/page/index.js
+++ src/page/index.js
@@ -1,9 +1,13 @@
 import { reducer as LoginReducer } from './login/index';
 import { reducer as HomeReducer } from './home/index';
+import { reducer as userReducer } from './user/index';
+import { reducer as productReducer } from './product/index';
 
 const reducer = {
   login: LoginReducer,
-  home: HomeReducer
+  home: HomeReducer,
+  user: userReducer,
+  ...productReducer,
 };
 
 export {
