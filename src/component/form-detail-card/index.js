50a02eba786ed3897f5a647e1856785fd2d72ee7
luohuidong <luohuidong01@126.com>
2019-02-01 15:16:24 +0800

    新增表单详情展示组件

---
+++ src/component/form-detail-card/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './style.scss';
+
+class FormDetailCard extends React.PureComponent {
+  createItems(data) {
+    return data.map(element => (
+      <div key={element.title}>
+        <span>{element.title}</span>：<span>{element.text}</span>
+      </div>  
+    ));
+  }
+  render() {
+    const { title, data, showBottom } = this.props;
+    
+    return (
+      <div>
+        <div className={styles['title']}>{title}</div>
+        { this.props.children }
+        {
+          data.length !== 0 &&
+          <div className={styles['grid-container']}>
+            {this.createItems(data)}
+          </div>
+        }
+        {
+          showBottom && <div className={styles.divider} />
+        }
+      </div>
+    );
+  }
+}
+
+FormDetailCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  data: PropTypes.array.isRequired,
+  showBottom: PropTypes.bool.isRequired,
+  children: PropTypes.element,
+};
+
+FormDetailCard.defaultProps = {
+  data: [],
+  showBottom: true
+};
+export default FormDetailCard;
