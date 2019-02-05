5c4222bf4772200da3153f70fa1fb13bfbd1587a
luohuidong <luohuidong01@126.com>
2019-02-05 21:09:04 +0800

    webpack production 打包添加 source-map

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -3,6 +3,7 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'production',
+  devtool: 'source-map',
   optimization: {
     splitChunks: {
       chunks: 'all'
