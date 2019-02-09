9b3aa31e36cac889da6bfc14df18528e6aba0527
luohuidong <luohuidong01@126.com>
2019-02-09 19:24:35 +0800

    新增 react-router location context，用于 menu 获取 location 信息

--- src/page/route.js
+++ src/page/route.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Switch, Route } from 'react-router-dom';
 
+import { LocationContext } from 'util/context.js';
 import Layout from 'component/layout/index.js';
 import Home from 'page/home/index.js';
 import ErrorPage from 'page/404/index.js';
@@ -12,18 +14,24 @@ import { Route as OrderRoute } from 'page/order';
 class PageRoute extends React.Component {
   render() {
     return (
-      <Layout>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path='/user' component={UserRoute} />
-          <Route path='/product' component={ProductRoute} />
-          <Route path='/category' component={CategoryRoute} />
-          <Route path='/order' component={OrderRoute} />
-          <Route component={ErrorPage} />
-        </Switch>
-      </Layout>
+      <LocationContext.Provider value={this.props.location}>
+        <Layout>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path='/user' component={UserRoute} />
+            <Route path='/product' component={ProductRoute} />
+            <Route path='/category' component={CategoryRoute} />
+            <Route path='/order' component={OrderRoute} />
+            <Route component={ErrorPage} />
+          </Switch>
+        </Layout>
+      </LocationContext.Provider>
     );
   }
 }
+
+PageRoute.propTypes = {
+  location: PropTypes.object
+};
 
 export default PageRoute;
