a7d44cd8771bf6f0c5a3247e24d481a8601e57b9
luohuidong <luohuidong01@126.com>
2019-02-09 19:11:05 +0800

    样式修改

--- src/component/layout/content/index.js
+++ src/component/layout/content/index.js
@@ -3,26 +3,14 @@ import PropTypes from 'prop-types';
 import { Layout } from 'antd';
 
 class Content extends React.Component {
-  render () {
+  render() {
     return (
-      <Layout.Content style={styles.layoutContent}>
-        <div style={styles.childrenContainer}>
-          {this.props.children}
-        </div>
+      <Layout.Content>
+        {this.props.children}
       </Layout.Content>
     );
   }
 }
-
-const styles = {
-  layoutContent: {
-    margin: 16
-  },
-  childrenContainer: { 
-    padding: 24, 
-    minHeight: 360 
-  }
-};
 
 Content.propTypes = {
   children: PropTypes.element
