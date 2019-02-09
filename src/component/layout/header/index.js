b8665163704fba3022e82444481431b30d7e9d39
luohuidong <luohuidong01@126.com>
2019-02-09 19:10:46 +0800

    样式修改

--- src/component/layout/header/index.js
+++ src/component/layout/header/index.js
@@ -4,17 +4,10 @@ import { Layout } from 'antd';
 import Dropdown from './dropdown';
 
 class Header extends React.PureComponent {
-  render () {
-    const divStyle = { 
-      width: '100%', 
-      display: 'flex', 
-      justifyContent: 'flex-end', 
-      paddingRight: 30 
-    };
-
+  render() {
     return (
-      <Layout.Header style={{ background: '#fff', padding: 0 }}>
-        <div style={divStyle}>
+      <Layout.Header style={styles.header}>
+        <div style={styles.dropContainer}>
           <Dropdown />
         </div>
       </Layout.Header>
@@ -22,8 +15,18 @@ class Header extends React.PureComponent {
   }
 }
 
-Header.propTypes = {
-
+const styles = {
+  header: {
+    position: 'relative', 
+    backgroundColor: '#ffffff',
+    boxShadow: '0 1px 4px rgba(0,21,41,.08)',
+  },
+  dropContainer: {
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'flex-end',
+    paddingRight: 30,
+  }
 };
 
 export default Header;
