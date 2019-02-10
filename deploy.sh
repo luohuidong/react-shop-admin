38e79b83fc7dbb845ae500c92b4bc2549e8f04a9
luohuidong <luohuidong01@126.com>
2019-02-10 19:35:33 +0800

    添加部署脚本

---
+++ deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+git pull
+
+if [ $? -eq 0 ]
+    then
+        echo '已获取最新的代码'
+        npm run build
+else
+    echo '获取最新代码失败'
+    exit
+fi
+
+if [ $? -eq 0 ] 
+    then 
+        echo '静态文件生成成功'
+        rm -rf /www/react-shop-admin/*
+else
+    echo '静态文件生成失败'
+    exit
+fi
+
+
+if [ $? -eq 0 ]
+    then
+        echo '清除旧静态文件成功'
+        cp -r build/* /www/react-shop-admin/
+else 
+    echo '清除旧文件失败'
+    exit
+fi
