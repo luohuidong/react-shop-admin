6fdcd96d8409fc2bdb521710440444d965a07a0b
luohuidong <luohuidong01@126.com>
2019-02-14 10:12:00 +0800

    添加 babel 配置文件

---
+++ babel.config.js
@@ -0,0 +1,16 @@
+module.exports = function (api) {
+  api.cache(true);
+
+  const presets = ['@babel/preset-env', '@babel/preset-react'];
+  const plugins = [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-transform-arrow-functions',
+    '@babel/plugin-transform-async-to-generator',
+    'react-hot-loader/babel'
+  ];
+
+  return {
+    presets,
+    plugins
+  };
+};
--- webpack/loader/babel.js
+++ webpack/loader/babel.js
@@ -3,14 +3,5 @@ module.exports = {
   exclude: /(node_modules|bower_components)/,
   use: {
     loader: 'babel-loader',
-    options: {
-      presets: ['@babel/preset-env', '@babel/preset-react'],
-      plugins: [
-        '@babel/plugin-proposal-class-properties',
-        '@babel/plugin-transform-arrow-functions',
-        '@babel/plugin-transform-async-to-generator',
-        'react-hot-loader/babel'
-      ]
-    }
   }
 };
