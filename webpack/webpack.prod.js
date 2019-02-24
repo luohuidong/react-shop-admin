0aff4b3949f37ae8b1c71a9e521759f31361aa46
luohuidong <luohuidong01@126.com>
2019-02-24 22:49:33 +0800

    添加 source map

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -7,7 +7,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
-  // devtool: 'source-map',
+  devtool: 'source-map',
   output: {
     filename: '[name].[hash].bundle.js',
   },
