3609cb372247181b7d7742f765a8770dc4ea2c61
luohuidong <luohuidong01@126.com>
2019-02-24 13:07:16 +0800

    页面改为按需加载

--- src/page/order/route.js
+++ src/page/order/route.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Loadable from 'react-loadable';
 
 import { orderRoute } from 'util/route';
+import PageLoading from 'component/page-loading/index';
 import ErrorPage from 'page/404/index';
-import OrderList from './list/index.js';
-import OrderDetail from './detail/index.js';
+
+const LoadableOrderList = Loadable({
+  loader: () => import('./list/index.js'),
+  loading: PageLoading,
+});
+
+const LoadableOrderDetail = Loadable({
+  loader: () => import('./detail/index.js'),
+  loading: PageLoading,
+});
 
 class ProductRoute extends React.Component {
   render() {
     return (
       <Switch>
-        <Route exact path={orderRoute.list} component={OrderList} />
-        <Route path={`${orderRoute.detail}/:orderId`} component={OrderDetail} />
+        <Route exact path={orderRoute.list} component={LoadableOrderList} />
+        <Route path={`${orderRoute.detail}/:orderId`} component={LoadableOrderDetail} />
         <Route component={ErrorPage} />
       </Switch>
     );
--- src/page/product/route.js
+++ src/page/product/route.js
@@ -1,20 +1,34 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Loadable from 'react-loadable';
 
 import { productRoute } from 'util/route';
+import PageLoading from 'component/page-loading/index';
 import ErrorPage from 'page/404/index';
-import ProductList from './list/index.js';
-import ProductEditor from './editor/index.js';
-import ProductDetail from './detail/index';
+
+const LoadableProductList = Loadable({
+  loader: () => import('./list/index'),
+  loading: PageLoading,
+});
+
+const LoadableProductEditor = Loadable({
+  loader: () => import('./editor/index'),
+  loading: PageLoading,
+});
+
+const LoadableProductDetail = Loadable({
+  loader: () => import('./detail/index'),
+  loading: PageLoading,
+});
 
 class ProductRoute extends React.Component {
   render() {
     return (
       <React.Fragment>
         <Switch>
-          <Route exact path={productRoute.list} component={ProductList} />
-          <Route path={`${productRoute.editor}/:productId?`} component={ProductEditor} />
-          <Route path={`${productRoute.detail}/:productId`} component={ProductDetail} />
+          <Route exact path={productRoute.list} component={LoadableProductList} />
+          <Route path={`${productRoute.editor}/:productId?`} component={LoadableProductEditor} />
+          <Route path={`${productRoute.detail}/:productId`} component={LoadableProductDetail} />
           <Route component={ErrorPage} />
         </Switch>
       </React.Fragment>
